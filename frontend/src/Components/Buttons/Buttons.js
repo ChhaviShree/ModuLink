e7781e48c4d7e3ea48da7211523cae809cff7d59
Chhavi <ansikachhavi003@gmail.com>
2024-02-02 15:32:06 +0530

    AddedComponentsfolder

---
+++ frontend/src/Components/Buttons/Buttons.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Buttons.css'
+const Buttons = () => {
+  return (
+    <div className='button'>
+      
+    </div>
+  )
+}
+
+export default Buttons
---
+++ frontend/src/Components/Models/Models.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Models.css'
+const Models = () => {
+  return (
+    <div className='models'>
+      
+    </div>
+  )
+}
+
+export default Models
