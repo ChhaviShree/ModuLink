01d392e8689e0edbada548d1a74368551c4dc509
Chhavi <ansikachhavi003@gmail.com>
2024-02-16 12:41:20 +0530

    AddedNewsletterandFooter

--- frontend/src/Components/NewsLetter/NewsLetter.js
+++ frontend/src/Components/NewsLetter/NewsLetter.js
@@ -4,7 +4,12 @@ import './NewsLetter.css'
 const NewsLetter = () => {
   return (
     <div className='newsletter'>
-      
+      <h1>GET EXCLUSIVE OFFERS ON YOUR EMAIL</h1>
+      <p>Subscribe to our NewsLetter and stay updated</p>
+    <div>
+      <input type='email' placeholder='Your Email Id' />
+    <button>Subscribe</button>
+    </div>
     </div>
   )
 }
