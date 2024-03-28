627eb5af11f67f9e8de707c1c17a8a32b29358d2
Chhavi <ansikachhavi003@gmail.com>
2024-03-28 13:17:19 +0530

    ChangesinNavbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,33 +1,53 @@
-import React, { useRef , useState} from 'react'
-import './Navbar.css'
-import logo from '../Assets/finallogo.png'
-import {Link} from 'react-router-dom'
-import nav_dropdown from '../Assets/dropdown_icon.png'
-
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../Assets/finallogo.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [menu,setMenu]=useState("home");
-  const menuRef=useRef();
-  const dropdown_toggle=(e)=>{
-    menuRef.current.classList.toggle('nav-menu-visible');
-    e.target.classList.toggle('open');
+  const [menu, setMenu] = useState("home");
+  const [showDropdown, setShowDropdown] = useState(false);
 
+  const handleLoginClick = () => {
+    setShowDropdown(!showDropdown);
   }
+
+  const handleLoginOptionClick = (option) => {
+    setMenu(option);
+    setShowDropdown(false);
+  }
+
   return (
     <div className='navbar'>
       <div className='nav-logo'>
-        <img src={logo} alt=""/>
+        <img src={logo} alt="" />
         <p>MODULINK</p>
       </div>
-      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt=""/>
-      <ul ref={menuRef} className='nav-menu'>
-        <li onClick={()=>{setMenu("home")}}><Link style={{textDecoration:'none' ,color:'white'}} to='/'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("about")}}><Link style={{textDecoration:'none',color:'white'}} to='/About'>About</Link>{menu==="about"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("blogs")}}><Link style={{textDecoration:'none',color:'white'}} to='/Blogs'>Blogs</Link>{menu==="blogs"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("CustomerLogin")}}><Link style={{textDecoration:'none',color:'white'}} to='/CustomerLogin'>CustomerLogin</Link>{menu==="CustomerLogin"?<hr/>:<></>}</li>
+      <ul className={`nav-menu`}>
+        <li onClick={() => { setMenu("home") }}>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to='/'>Home</Link>
+          {menu === "home" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => { setMenu("about") }}>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to='/About'>About</Link>
+          {menu === "about" ? <hr /> : <></>}
+        </li>
+        <li onClick={() => { setMenu("blogs") }}>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to='/Blogs'>Blogs</Link>
+          {menu === "blogs" ? <hr /> : <></>}
+        </li>
+        <li>
+          <span className="login-option" onClick={handleLoginClick}>Login</span>
+          {showDropdown && (
+            <div className="dropdown-menu">
+              <button className="dropdown-item" onClick={() => handleLoginOptionClick("CustomerLogin")}>Customer Login</button>
+              <button className="dropdown-item" onClick={() => handleLoginOptionClick("VendorLogin")}>Vendor Login</button>
+            </div>
+          )}
+          {(menu === "CustomerLogin" || menu === "VendorLogin") && <hr />}
+        </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/Components/NewsLetter/NewsLetter.js
+++ frontend/src/Components/NewsLetter/NewsLetter.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './NewsLetter.css'
 
 const NewsLetter = () => {
+  const [email,setEmail]=useState('');
+
+  const handleSubscribe=()=>{
+    console.log('Subscribed with email: ',email);
+    setEmail('');
+  };
+
   return (
     <div className='newsletter'>
       <h1>GET EXCLUSIVE OFFERS ON YOUR EMAIL</h1>
       <p>Subscribe to our NewsLetter and stay updated</p>
     <div>
-      <input type='email' placeholder='Your Email Id' />
-    <button>Subscribe</button>
+      <input type='email' placeholder='Your Email Id' 
+      value={email}
+      onChange={(e)=>setEmail(e.target.value)}
+      />
+    <button onClick={handleSubscribe}>Subscribe</button>
     </div>
     </div>
   )
