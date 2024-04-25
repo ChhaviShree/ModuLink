300c0b0842cdfec9d6ff9edb3e3e8b5154fba073
Sachit <sachittarway2002@gmail.com>
2024-04-25 08:08:54 +0530

    Check

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,45 @@
-const express=require('express');
-const cors=require('cors');
-const {db} =require('./db/db');
+const express = require("express");
+const cors = require("cors");
+const { db } = require("./db/db");
 // const {readdirSync}=require('fs');
-const vendorViewRouter = require('./routes/vendorViewRoutes');
+const vendorViewRouter = require("./routes/vendorViewRoutes");
 const vendorRouter = require("./routes/vendorRoutes");
-const customerRouter=require("./routes/CustomerRoutes");
+const customerRouter = require("./routes/CustomerRoutes");
 // const blogRouter = require("./routes/blogRoutes");
 
+const app = express();
 
-const app=express();
+require("dotenv").config();
 
-require('dotenv').config();
+const PORT = process.env.PORT;
 
-const PORT=process.env.PORT
-
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-
 
 //routes
 
 // readdirSync('./routes').map((route)=> app.use('/api/v1', require('./routes/'+ route)))
 
-app.use("/vendors",vendorRouter);
-app.use("/customers",customerRouter);
-app.use("/vendor-details",vendorViewRouter);
+app.use("/vendors", vendorRouter);
+app.use("/customers", customerRouter);
+app.use("/vendor-details", vendorViewRouter);
 
 // app.use("/blogs",blogRouter);
 
 app.get("/", (req, res) => {
-    res.send("Success");
+  res.send("Success");
 });
 
-app.use((err,req,res,next)=>{
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!!");
 });
 
-const server=()=>{
-    db()
-    app.listen(PORT,()=>{
-        console.log('Server is running on port',PORT);
-    })
-    
-}
+const server = () => {
+  db();
+  app.listen(PORT, () => {
+    console.log("Server is running on port", PORT);
+  });
+};
 
-server()
+server();
