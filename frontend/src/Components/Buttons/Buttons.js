3ff287c931e3d09f25c20a1b43016114c50bbd03
Sachit <sachittarway2002@gmail.com>
2024-05-04 10:00:49 +0530

    Updated Navigation

--- frontend/src/Components/Buttons/Buttons.js
+++ frontend/src/Components/Buttons/Buttons.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Buttons.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Buttons.css";
 
 const Buttons = () => {
   return (
-    <div className='button'>
-      <div className='button-item'>
-        <Link to="/build-your-house">Build Your House</Link>
+    <div className="button">
+      <div className="button-item">
+        <Link to="/signup">Build Your House</Link>
       </div>
-      <div className='button-item'>
+      <div className="button-item">
         <Link to="/vendor-register">Vendor Registration</Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
